6a5302056c47f8d688164d0b8cbcb31edc347e65
Andrés González <afgonz@users.noreply.github.com>
2025-01-20 16:59:06 +1100

    Added basic enemy behaviour

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,35 +1,34 @@
 using UnityEngine;
-using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
     public DangerZone dangerZone;
 
-    private Coroutine goToTarget;
     private Vector3 target = Vector3.zero;
-    private float offset = 0.1f;
+    private float threshold = 0.1f;
+    [SerializeField] private float speed = 1f;
 
     public void Initialise(DangerZone dangerZone)
     {
         this.dangerZone = dangerZone;
-        if (goToTarget != null)
+        target = CalculateNewTarget(dangerZone.transform.position, dangerZone.radius);
+    }
+
+    private Vector3 CalculateNewTarget(Vector3 origin, float radius)
+    {
+        return (Vector3)(Random.insideUnitCircle * radius) + origin;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target) > threshold)
         {
-            StopCoroutine(goToTarget);
+            var step = speed * Time.deltaTime; 
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
         }
-        goToTarget = StartCoroutine(GoToTarget());
-    }
-
-    private void CalculateTarget()
-    {
-
-    }
-
-    private IEnumerator GoToTarget()
-    {
-        while(Vector3.Distance(transform.position, target) > 0.1f)
+        else
         {
-            
-            yield return null;
+            target = CalculateNewTarget(dangerZone.transform.position, dangerZone.radius);
         }
     }
 }
